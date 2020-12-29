1e487a2e0a98285440691ba02f13a92cb730c7f4
Bryan Bugyi <bryanbugyi34@gmail.com>
2020-12-29 18:28:57 -0500

    Blacken setup.py

--- setup.py
+++ setup.py
@@ -1,48 +1,47 @@
 # -*- coding: utf-8 -*-
-"""
-clipster - python clipboard manager
-"""
 
 from setuptools import find_packages, setup
 
-dependencies = []
-setup_requires = ['setuptools_scm']
 
 setup(
-    name = "clipster",
-    setup_requires=setup_requires,
+    name="clipster",
+    setup_requires=["setuptools_scm"],
     use_scm_version=True,
-    url = "https://github.com/mrichar1/clipster",
-    license='LICENSE.md',
-    author = "Matthew Richardson",
-    author_email = "",
-    description='python clipboard manager',
+    url="https://github.com/mrichar1/clipster",
+    license="LICENSE.md",
+    author="Matthew Richardson",
+    author_email="",
+    description="python clipboard manager",
     long_description=__doc__,
-    packages=find_packages(exclude=['tests']),
+    packages=find_packages(exclude=["tests"]),
     include_package_data=True,
-    data_files = [("share/licenses/clipster", ["LICENSE.md"]), ("share/doc/clipster", ["README.md"])],
+    data_files=[
+        ("share/licenses/clipster", ["LICENSE.md"]),
+        ("share/doc/clipster", ["README.md"]),
+    ],
     zip_safe=False,
-    platforms='any',
-    install_requires=dependencies,
-    scripts = ['clipster'],
+    platforms="any",
+    install_requires=(),
+    scripts=["clipster"],
     classifiers=[
         # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
         # 'Development Status :: 1 - Planning',
         # 'Development Status :: 2 - Pre-Alpha',
         # 'Development Status :: 3 - Alpha',
         # 'Development Status :: 4 - Beta',
-        'Development Status :: 5 - Production/Stable',
+        "Development Status :: 5 - Production/Stable",
         # 'Development Status :: 6 - Mature',
         # 'Development Status :: 7 - Inactive',
-        'Environment :: Console',
-        'Environment :: X11 Applications',
-        'Intended Audience :: End Users/Desktop',
-        'License :: OSI Approved :: "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
-        'Operating System :: POSIX',
-        'Operating System :: MacOS',
-        'Operating System :: Unix',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
-    ]
+        "Environment :: Console",
+        "Environment :: X11 Applications",
+        "Intended Audience :: End Users/Desktop",
+        'License :: OSI Approved :: "License :: OSI Approved :: GNU Affero'
+        " General Public License v3 or later (AGPLv3+)",
+        "Operating System :: POSIX",
+        "Operating System :: MacOS",
+        "Operating System :: Unix",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
 )
